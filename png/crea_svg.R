be6c8507aa4d80c76a3d4c819bea2a40810d78d3
Josep Pueyo-Ros <jpueyo@icra.cat>
2024-07-22 09:38:34 +0200

    create a single png

---
+++ png/crea_svg.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(xml2)
+
+files <- c(
+  file.path("..", "assets", "climate_classifications", list.files("assets/climate_classifications/")),
+  file.path("..", "assets", "suitability", list.files("assets/suitability/")),
+  file.path("..", "assets", "suitability_irrig", list.files("assets/suitability_irrig/")),
+  file.path("..", "assets", "nbr", list.files("assets/nbr/"))
+)
+
+size <- 200
+width <- size * 3
+height <- size * length(files)/3 
+
+coords <- tibble(
+  x = rep(seq(0, width - size, size), length(files)/3),
+  y = rep(seq(0, height - size, size), each = 3),
+  file = files
+)
+
+g <- read_xml(glue::glue('<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg"></svg>'))
+
+pwalk(coords, \(x, y, file) g |>
+  xml_add_child(
+    "image", 
+    width = size, heigth = size, x = x, y = y,
+    href = file)
+)
+  
+g |> write_xml("png/test.svg")
